c28b7ec70143459060df912350fcb57febb04c15
Avijit Singh <singhavijit395@gmail.com>
2025-09-22 23:29:20 +0530

    Update app.py

--- project/app.py
+++ project/app.py
@@ -9,4 +9,5 @@ register_routes(app)
 
 if __name__ == "__main__":
     start_imap_polling()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
+
